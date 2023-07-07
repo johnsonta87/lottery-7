1e75192c5a20ac41bbf7ef1fb9aae0b425efab28
Johnson <johnsonta87@gmail.com>
2023-07-07 16:18:17 -0400

    styling update

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     return (
       <div
         key={index}
-        className="font-bold text-white rounded-full bg-purple-500 text-lg flex items-center justify-center mx-4 text-center" style={{
+        className="font-bold text-white rounded-full bg-purple-500 text-lg flex items-center justify-center text-center font-mono" style={{
           width: `40px`,
           height: `40px`
         }}>
@@ -43,27 +43,31 @@ function App() {
   })
 
   return (
-    <div className="bg-orange-100 p-24 rounded-xl">
-      <h1 className="text-pink-600 text-6xl font-bold mb-8">Lottery 7</h1>
+    <div className="bg-orange-100 p-4 md:p-16 rounded-xl ease-in duration-300 drop-shadow-md hover:drop-shadow-2xl">
+      <h1 className="text-rose-600 text-3xl md:text-6xl font-bold mb-8">Lottery 7</h1>
 
-      <div className="flex justify-between my-4" style={{width: `500px`}}>
-        {loading ? <p>Thinking ...</p> : numbersList}
+      <div className="flex flex-wrap justify-center my-4 w-full gap-2 md:gap-6">
+        {loading ? <p className="text-blue-600 font-bold">I am thinking ...</p> : numbersList}
       </div>
 
-      <div className="flex flex-col">
-        <button
-          type="button"
-            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full mt-4 px-8 uppercase font-bold"
-          onClick={() => handleClick()}>
-            Click to Pick
-          </button>
+      {loading ? null :
+        <div className="flex flex-col md:flex-row gap-4 mt-8 justify-center">
           <button
-            type="button"
-            className="bg-sky-600 text-white rounded-full mt-4 px-8 uppercase font-bold"
-          onClick={() => setNumbers([])}>
-            Clear
-          </button>
-        </div>
+              type="button"
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 border-2 border-gray-500 text-white rounded-full px-8 uppercase font-bold ease-in duration-300 drop-shadow-md hover:drop-shadow-2xl"
+              onClick={() => handleClick()}>
+              Click to Pick
+            </button>
+          {numbersList && numbersList.length ?
+            <button
+              type="button"
+              className="bg-amber-300 text-slate-500 border-2 border-gray-500 rounded-full px-8 uppercase font-bold ease-in duration-300 drop-shadow-md hover:drop-shadow-2xl"
+              onClick={() => setNumbers([])}>
+              Clear
+            </button>
+          : null}
+          </div>
+        }
     </div>
   )
 }
